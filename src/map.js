f2a36adea1c0626a2362214a7b0b85a7142f22e9
NithishGajula <nithishg1199@gmail.com>
2021-08-30 22:47:26 +0530

    Edits

--- src/map.js
+++ src/map.js
@@ -1,10 +1,5 @@
 const map = (array, call) => {
-    if( call == "cube")
-        return array.map((x) => {return x*x*x;});
-    else if( call == "identity")
-        return array.map((x) => {return x;})
-    else
-        return null;
+    return array.map(call);
 }
 
 module.exports = map;
--- src/map.spec.js
+++ src/map.spec.js
@@ -3,18 +3,23 @@ const map = require('./map');
 describe('map', () => {
 
     it('map([1,2,3], cube) should give [1,8,27]', () => {
-        const res = map([1, 2, 3], "cube");
+        const res = map([1, 2, 3], x => x*x*x);
         expect(res).toEqual([1, 8, 27]);
     });
 
     it('map([], cube) should give []', () => {
-        const res = map([], "cube");
+        const res = map([], x => x*x*x);
         expect(res).toEqual([]);
     });
 
     it('map([1,2,3], identity) should give [1,2,3]', () => {
-        const res = map([1, 2, 3], "identity");
+        const res = map([1, 2, 3], x => x);
         expect(res).toEqual([1, 2, 3]);
     });
 
+    it('map([a{x : 10}],someObject => someObject.x + 1) -> [11]', () => {
+        const res = map([{x : 10}], a => a.x + 1);
+        expect(res).toEqual([11]);
+    });
+    
 })
